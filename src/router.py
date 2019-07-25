b2cd56cd1ad0ce6bde5123e8d93333f91919737e
Yuan Lu <yl768@njit.edu>
2019-07-25 19:56:32 -0400

    my tcp server

--- src/router.py
+++ src/router.py
@@ -1,33 +1,53 @@
-from node import Recv, Send
+from node import Recv, Send, Server
 import socketserver
+import multiprocessing
 from multiprocessing import Process
+from multiprocessloghandler import MultiprocessHandler
+import logging, sys
+
 import time
 
+def get_log():
+    formattler = '%(levelname)s - %(name)s - %(asctime)s - %(message)s'
+    fmt = logging.Formatter(formattler)
+    logger = logging.getLogger()
+    #logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(fmt)
+    #file_handler = MultiprocessHandler('mylog', when='M')
+    #file_handler.setLevel(logging.DEBUG)
+    #file_handler.setFormatter(fmt)
+    logger.addHandler(stream_handler)
+    #logger.addHandler(file_handler)
+    return logger
+
+
 def start_recv(address):
-    server = socketserver.TCPServer(address, Recv)
+    server = Server(address, Recv, get_log)
     server.serve_forever()
 
 
-if __name__ == "__main__":
+def test():
+    #global logger
+    #logger = get_log()
 
     N = 4
-    addresses=[None]*4
-
-    sends=[None]*4
     host = "localhost"
     port_base = 12000
 
+    addresses = [(host, port_base + i) for i in range(N)]
+    sends = [Send(i, addresses) for i in range(N)]
 
     for i in range(N):
-        addresses[i] = (host, port_base + i)
-
-    for i in range(N):
-        sends[i] = Send(i, addresses)
-        Process(target=start_recv, kwargs={"address":addresses[i]}).start()
+        Process(target=start_recv, kwargs={"address": addresses[i]}).start()
 
     for j in range(10):
         for i in range(N):
-            print('node %d' % i)
             msg = ("msg %d" % j).encode('utf-8')
-            sends[j%4].send(addresses[i], msg)
+            sends[j % 4].send(addresses[i], msg)
             time.sleep(1)
+
+
+if __name__ == "__main__":
+    test()
